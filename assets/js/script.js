351f6d6b3c26e6472c2ea51122c3a873845c876c
patfern <patrick.j.fernbach@gmail.com>
2022-01-19 21:54:40 -0500

    UX enhancements

--- assets/js/script.js
+++ assets/js/script.js
@@ -75,14 +75,22 @@ $(".card .list-group").sortable({
   helper: "clone",
   activate: function(event, ui) {
     console.log(ui);
+    $(this).addClass("dropover");
+    $(".bottom-trash").addClass("bottom-trash-drag");
   },
   deactivate: function(event, ui) {
     console.log(ui);
+    $(this).removeClass("dropover");
+    $(".bottom-trash").removeClass("bottom-trash-drag");
   },
   over: function(event) {
+    $(this).addClass("dropover-active");
+    $(".bottom-trash-drag").addClass("bottom-trash-active");
     console.log(event);
   },
   out: function(event) {
+    $(this).removeClass("dropover-active");
+    $(".bottom-trash").removeClass("bottom-trash-active")
     console.log(event);
   },
   update: function() {
@@ -153,7 +161,7 @@ $("#task-form-modal").on("shown.bs.modal", function() {
 });
 
 // save button in modal was clicked
-$("#task-form-modal .btn-primary").click(function() {
+$("#task-form-modal .btn-save").click(function() {
   // get form values
   var taskText = $("#modalTaskDescription").val();
   var taskDate = $("#modalDueDate").val();
@@ -279,3 +287,8 @@ $("#remove-tasks").on("click", function() {
 // load tasks for the first time
 loadTasks();
 
+setInterval(function() {
+  $(".card .list-group-item").each(function(index, el){
+    auditTask(el);
+  });
+},1800000 );
